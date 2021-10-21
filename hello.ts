dfa4bc4ce9092528dcddad63fd78b27821a0c99f
AnnSatheesh <92846581+AnnSatheesh@users.noreply.github.com>
2021-10-21 23:17:22 +0530

    Update hello.ts

--- hello.ts
+++ hello.ts
@@ -1,13 +1,1 @@
-//hello.ts
-class Greeter {
-    greeting: string;
-    constructor() {
-        this.greeting = "World";
-    }
-    greet() {
-        return "Hello, " + this.greeting;
-    }
-}
-
-let greeter = new Greeter();
-console.log(greeter.greet());
+hello guys hope you are ok
